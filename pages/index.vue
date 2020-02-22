a1210454fda4301cc7ccdb8f1c109bf0ccc429c2
Po Rith <poamrong@rith.com>
2020-02-22 10:50:36 -0800

    staging commit

--- pages/index.vue
+++ pages/index.vue
@@ -15,16 +15,4 @@ export default {
 body, html {
   height: 100%;
 }
-
-body {
-  background-image: url("/angkor.gif");
-  height: 100%;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: cover;
-}
-
-img {
-  /* max-height: 500px; */
-}
 </style>
