a57778e79dc9fe45e070a85f95a1afd157a28bf1
Po Rith <poamrong@rith.com>
2020-02-21 20:23:20 -0800

    commit

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="container pt-5">
+      <div class="row">
+        <div class="offset-md-3 col-md-6 text-center">
+          <img src="/performer-01.jpg" alt="">
+        </div>
+        <!-- <div class="col-md-7"> -->
+          <!-- <h1>Khmer New Year 2020</h1> <br><br>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex modi obcaecati id expedita nostrum minima? Velit ipsa dolorum repudiandae id, consequuntur quibusdam reprehenderit expedita omnis nobis quidem eius laborum repellendus quod. Nostrum corporis eveniet ullam molestias soluta cupiditate, tempora maxime.
+          <br><br> -->
+        <!-- </div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+img {
+  max-height: 500px;
+}
+</style>
