81a8d90dfef420bb64d6d6e689994a1a89d8df3e
Po Rith <poamrong@rith.com>
2020-02-22 14:15:59 -0800

    update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,71 +2,98 @@ const pkg = require('./package')
 
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Khmer New Year 2020 | Oakland, CA',
+    title: "Khmer New Year 2020 | Oakland, CA",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "title", content: "Khmer New Year 2020 | Oakland, CA" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "We sincerely invite you to join us for the 30th annual celebration of Cambodian (Khmer) New Year celebration, hosted by the Oakland Cambodian Dance Troupe, at the LDS Oakland California Temple. Join us for a night complete with traditional dance performances, music, food and games."
+      },
+      { property: "og:type", content: "website" },
+      { property: "og:url", content: "https://kny2020.com/" },
+      { property: "og:title", content: "Khmer New Year 2020 | Oakland, CA" },
+      { property: "og:description", content: "We sincerely invite you to join us for the 30th annual celebration of Cambodian (Khmer) New Year celebration, hosted by the Oakland Cambodian Dance Troupe, at the LDS Oakland California Temple. Join us for a night complete with traditional dance performances, music, food and games." },
+      { property: "og:image", content: "https://kny2020.com/performer-05.jpg" },
+      { property: "twitter:card", content: "summary_large_image" },
+      { property: "twitter:url", content: "https://kny2020.com/" },
+      { property: "twitter:title", content: "Khmer New Year 2020 | Oakland, CA" },
+      { property: "twitter:description", content: "We sincerely invite you to join us for the 30th annual celebration of Cambodian (Khmer) New Year celebration, hosted by the Oakland Cambodian Dance Troupe, at the LDS Oakland California Temple. Join us for a night complete with traditional dance performances, music, food and games." },
+      { property: "twitter:image", content: "https://kny2020.com/performer-05.jpg" }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      { rel: "stylesheet", type: "text/css", href: "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" },
-      { rel: "stylesheet", type: "text/css", href: 'https://fonts.googleapis.com/css?family=Cabin&display=swap' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+      },
+      {
+        rel: "stylesheet",
+        type: "text/css",
+        href: "https://fonts.googleapis.com/css?family=Cabin&display=swap"
+      }
     ],
     script: [
-      { src: 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js' },
-      { src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js' },
-      { src: 'https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js' }
+      {
+        src:
+          "https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"
+      },
+      { src: "https://code.jquery.com/jquery-3.3.1.slim.min.js" },
+      {
+        src:
+          "https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
+    "@nuxtjs/axios"
     // '@nuxtjs/pwa'
   ],
   /*
-  ** Axios module configuration
-  */
+   ** Axios module configuration
+   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
   },
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-      
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
