eaf030c4237f4b09c89c673ae5ab6f0b30e5d4a0
Po Rith <poamrong@rith.com>
2020-02-22 10:48:58 -0800

    stagin commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,7 @@
 <template>
   <div>
-    <!-- <img src="angkor-ani.gif" class="h-100" alt=""> -->
-    <!-- <div class="container pt-5">
-      <div class="row">
-        <div class="col-md-12 text-center">
-          Cambodian New Year 2020 <br><br>
-          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-target="#carouselExampleControls" data-slide-to="0" class="active"></li>
-              <li data-target="#carouselExampleControls" data-slide-to="1"></li>
-              <li data-target="#carouselExampleControls" data-slide-to="2"></li>
-              <li data-target="#carouselExampleControls" data-slide-to="3"></li>
-              <li data-target="#carouselExampleControls" data-slide-to="4"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="/performer-01.jpg"  class="d-block mx-auto" alt="First slide">
-              </div>
-              <div class="carousel-item">
-                <img src="/performer-02.jpg" class="d-block mx-auto" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img src="/performer-03.jpg" class="d-block mx-auto" alt="Third slide">
-              </div>
-              <div class="carousel-item">
-                <img src="/performer-04.jpg" class="d-block mx-auto" alt="Second slide">
-              </div>
-              <div class="carousel-item">
-                <img src="/performer-05.jpg" class="d-block mx-auto" alt="Third slide">
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-      </div> -->
-      <!-- <div class="row">
-        <div class="col-md-12 pt-4 text-center">
-          © 2020
-        </div>
-      </div> -->
+    <div>
+      Staging
     </div>
   </div>
 </template>
