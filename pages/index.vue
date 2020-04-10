2369623ce8bb12c2e48b97f8734c0190122650b6
Po Rith <poamrong@rith.com>
2020-04-10 10:28:26 -0700

    add CV-19 prompt

--- pages/index.vue
+++ pages/index.vue
@@ -187,6 +187,11 @@
 
 <script>
 export default {
+  mounted() {
+    setTimeout(() => {
+      alert('Unfortunately, this event has been post-poned due to: \n\nCORONA-VIRUS \n\nWe apologize for the inconvenience and hope to update you as new information is received.')
+    }, 2000)
+  },
   methods: {
     scrollToAddress: function () {
       let elTopPos = this.$refs['location-address'].getBoundingClientRect().top
